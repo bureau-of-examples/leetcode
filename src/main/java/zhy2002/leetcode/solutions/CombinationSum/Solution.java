91f5acc69aad58d18737f6afa4e18f2fb135775a
hz141@uowmail.edu.au <hz141@uowmail.edu.au>
2015-03-01 14:14:21 +1100

    Added solution to Combination Sum.

---
+++ src/main/java/zhy2002/leetcode/solutions/CombinationSum/Solution.java
@@ -0,0 +1,75 @@
+package zhy2002.leetcode.solutions.CombinationSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://oj.leetcode.com/problems/combination-sum/
+ */
+public class Solution {
+
+    private int[] candidates;
+    private int target;
+    private List<Integer> path; //current path
+    int sum;
+    List<List<Integer>> result;
+
+    //assume there are not duplicates in candidates
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates == null)
+            return null;
+
+        //initialization
+        Arrays.sort(candidates);
+        this.candidates = candidates;
+        this.target = target;
+        this.path = new ArrayList<>();
+        sum = 0;
+        result = new ArrayList<>();
+
+        findCombinationSum(0);
+        return result;
+    }
+
+    /**
+     * Check if the current path is a solution. If not try add node.
+     * When this is called sum is guaranteed to be <= target.
+     * @param nodeIndex Index in candidates array. nodeIndex is the first index >= all indices in the current path.
+     * @return return false if sum >= target.
+     */
+    private boolean findCombinationSum(int nodeIndex){
+
+        if(sum > target)
+            return false;
+
+        if(sum == target){
+            result.add(new ArrayList<Integer>(path));
+            return false;
+        }
+
+//        int diff = target - sum;
+//        //binary search for the first bigger than diff
+//        int start = 0, end = candidates.length - 1;
+//        while (start <= end){
+//            int mid = (start + end) >>> 1;
+//            if(candidates[mid] <= diff){
+//                start = mid + 1;
+//            } else {
+//                end = mid - 1;
+//            }
+//        }
+
+        for(int i=nodeIndex;/*avoid duplicates*/ i<candidates.length; i++){
+            path.add(candidates[i]);
+            sum += candidates[i];
+            boolean continueExpand = findCombinationSum(i);
+            path.remove(path.size() - 1);
+            sum -= candidates[i];
+            if(!continueExpand)
+                break;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/zhy2002/leetcode/tests/CombinationSumTests.java
@@ -0,0 +1,45 @@
+package zhy2002.leetcode.tests;
+
+import org.junit.Test;
+import zhy2002.leetcode.Utils;
+import zhy2002.leetcode.solutions.CombinationSum.Solution;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CombinationSumTests {
+
+    @Test
+    public void basicTest(){
+        Solution solution = new Solution();
+
+        List<List<Integer>> result = solution.combinationSum(new int[]{}, 0);
+        assertEquals("[[]]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{3, 1}, 5);
+        assertEquals("[[1,1,1,1,1],[1,1,3]]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{8, 9}, 5);
+        assertEquals("[]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{8, 9}, 0);
+        assertEquals("[[]]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{8, 9}, 15);
+        assertEquals("[]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{8, 9}, 19);
+        assertEquals("[]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{8, 9, 15}, 21);
+        assertEquals("[]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{4,8,7,3}, 10);
+        assertEquals("[[3,3,4],[3,7]]", Utils.listOfListToString(result));
+
+        result = solution.combinationSum(new int[]{9,6,8,11,5,4}, 34);
+        assertEquals("[[4,4,4,4,4,4,4,6],[4,4,4,4,4,4,5,5],[4,4,4,4,4,5,9],[4,4,4,4,4,6,8],[4,4,4,4,5,5,8],[4,4,4,4,6,6,6],[4,4,4,4,9,9],[4,4,4,5,5,6,6],[4,4,4,5,6,11],[4,4,4,5,8,9],[4,4,4,6,8,8],[4,4,4,11,11],[4,4,5,5,5,5,6],[4,4,5,5,5,11],[4,4,5,5,8,8],[4,4,5,6,6,9],[4,4,6,6,6,8],[4,4,6,9,11],[4,4,8,9,9],[4,5,5,5,5,5,5],[4,5,5,5,6,9],[4,5,5,6,6,8],[4,5,5,9,11],[4,5,6,8,11],[4,5,8,8,9],[4,6,6,6,6,6],[4,6,6,9,9],[4,6,8,8,8],[4,8,11,11],[5,5,5,5,5,9],[5,5,5,5,6,8],[5,5,5,8,11],[5,5,6,6,6,6],[5,5,6,9,9],[5,5,8,8,8],[5,6,6,6,11],[5,6,6,8,9],[5,9,9,11],[6,6,6,8,8],[6,6,11,11],[6,8,9,11],[8,8,9,9]]", Utils.listOfListToString(result));
+
+    }
+}
