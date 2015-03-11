b42795252dad2d14e4b39abd8e97ca21c11754bc
hz141@uowmail.edu.au <hz141@uowmail.edu.au>
2015-03-11 23:41:24 +1100

    To implement Word Ladder.

---
+++ src/main/java/zhy2002/leetcode/solutions/wordladder/Solution.java
@@ -0,0 +1,15 @@
+package zhy2002.leetcode.solutions.wordladder;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/word-ladder/
+ */
+public class Solution {
+
+    public int ladderLength(String start, String end, Set<String> dict) {
+        throw new NotImplementedException();
+    }
+}
