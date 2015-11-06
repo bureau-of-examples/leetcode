3a86722bb9d264ceffb0a7630517f34ddcb66808
zhy2002 <hz141@uowmail.edu.au>
2015-11-06 17:49:54 +1100

    todo this one use two pointers.

---
+++ src/main/java/zhy2002/leetcode/solutions/strobogrammaticnumber/Solution.java
@@ -0,0 +1,15 @@
+package zhy2002.leetcode.solutions.strobogrammaticnumber;
+
+/**
+ * https://leetcode.com/problems/strobogrammatic-number/
+ * A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+ * rite a function to determine if a number is strobogrammatic. The number is represented as a string.
+ * For example, the numbers "69", "88", and "818" are all strobogrammatic.
+ */
+public class Solution {
+
+    public boolean isStrobogrammatic(String num) {
+
+
+    }
+}
