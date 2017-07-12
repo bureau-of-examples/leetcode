e5238fed4c7bacb17fc84ef8ad3f1be5b503520e
john.zhang <john.zhang@rubik.com.au>
2017-07-12 21:52:05 +1000

    fix test error.

--- src/test/java/zhy2002/leetcode/tests/FindDuplicateFileInSystemTests.java
+++ src/test/java/zhy2002/leetcode/tests/FindDuplicateFileInSystemTests.java
@@ -21,7 +21,7 @@ public class FindDuplicateFileInSystemTests {
         Solution solution = new Solution();
         assertThat(
                 solution.findDuplicate(new String[]{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"}).toString(),
-                equalTo("")
+                equalTo("[[root/a/2.txt, root/c/d/4.txt, root/4.txt], [root/a/1.txt, root/c/3.txt]]")
         );
     }
 
