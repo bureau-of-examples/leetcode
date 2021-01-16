ae675c4d250c1559cfbccf7fbdf89a08122b8779
Hanyang Zhang <hyz@amazon.com>
2021-01-17 10:51:32 +1100

    update params.

--- src/main/java/zhy2002/trading/test/SMATurn2Setup.java
+++ src/main/java/zhy2002/trading/test/SMATurn2Setup.java
@@ -1,5 +1,6 @@
 package zhy2002.trading.test;
 
+import zhy2002.trading.StockGroup;
 import zhy2002.trading.condition.And;
 import zhy2002.trading.condition.DaysBlowSMA;
 import zhy2002.trading.condition.Or;
@@ -14,19 +15,25 @@ import java.util.List;
 
 public class SMATurn2Setup extends BackTestSetup {
 
+    public List<StockGroup> createStockGroups() {
+        return List.of(
+                new StockGroup("AU", List.of("RIO.AX"))
+        );
+    }
+
     @Override
     public List<StrategyPair> createStrategyPairs() {
         var buys = new StrategyGeneratorV2(
                 "SMATurn2",
                 new ParameterCrossProduct()
-                        .withParameter("smaPeriods", new int[]{9, 14, 20})
-                        .withParameter("downWindow", new int[]{9, 10, 12})
-                        .withParameter("upWindow", new int[]{3, 5, 6})
-                        .withParameter("downRate", new double[]{0, -0.005, -0.007, -0.01, -0.011})
-                        .withParameter("upRate", new double[]{0, 0.005, 0.007, 0.01, 0.011})
-                        .withParameter("trendPeriods", new int[]{50})
-                        .withParameter("trendWindow", new int[]{20, 30})
-                        .withParameter("threshold", new double[]{0.7, 0.8, 0.9}),
+                        .withParameter("smaPeriods", new int[]{14})
+                        .withParameter("downWindow", new int[]{12, 13})
+                        .withParameter("upWindow", new int[]{3, 4, 5})
+                        .withParameter("downRate", new double[]{0, -0.003, -0.005})
+                        .withParameter("upRate", new double[]{0.005, 0.004, 0.003})
+                        .withParameter("trendPeriods", new int[]{20, 30})
+                        .withParameter("trendWindow", new int[]{30})
+                        .withParameter("threshold", new double[]{0.75, 0.8, 0.9}),
                 ps -> new And(
                         new SMATurn(
                                 ps.getInt("smaPeriods"),
@@ -40,7 +47,6 @@ public class SMATurn2Setup extends BackTestSetup {
                                 ps.getInt("trendWindow"),
                                 ps.getDouble("threshold")
                         )
-
                 ));
         var sells = new StrategyGeneratorV2(
                 "TrailingStopLoss",
